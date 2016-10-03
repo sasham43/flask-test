06987d14c9967513438fa88eba5b7dff9039ea1b
sasham43 <sashasemail@gmail.com>
2016-10-03 15:48:18 -0500

    working my way up

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+from flask.ext.sqlalchemy import SQLAlchemy
 
-from app import views
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+from app import views, models
