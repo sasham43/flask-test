c286f75f8985aec96062b3de70863e7a00f48df5
sasham43 <sashasemail@gmail.com>
2016-10-03 11:38:46 -0500

    looping in templates

--- app/views.py
+++ app/views.py
@@ -1,10 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Greetings World! It is I, the marvelous masticator!  Watch as I chew some stuff!'
-
-@app.route('/bingo')
-def bingo():
-    return 'Bing time!!!'
+    user = {'nickname': 'Miguel'}
+    posts = [
+        {
+            'author': {'nickname': 'Rodrigo'},
+            'body': 'Once I ate a clam, but it was not good.  Then I ate a cheese, and it was good.'
+        },
+        {
+            'author': {'nickname': 'Donnarumma'},
+            'body': 'Legends tell of a portly masseuse named Gene who will one day rule the world.'
+        }
+    ]
+    return render_template('index.html', subtitle='A City for Flasks', user=user, posts=posts)
